4a49d3f715d960e1965c9ed45db98e2705b1f465
lucky <jakub.krhovjak@protonmail.com>
2018-04-01 13:58:57 +0200

    first commit

---
+++ src/main/java/com/example/nangutask/NanguTaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.nangutask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanguTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanguTaskApplication.class, args);
+	}
+}
